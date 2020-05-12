a9d17054a822396e1aecb94b315f38b7358ed138
bahrikutlu <bahrikutlu@gmail.com>
2020-05-12 14:59:52 -0500

    PostgreSQL with Django Tutorial https://www.enterprisedb.com/postgres-tutorials/how-use-postgresql-django
    https://docs.djangoproject.com/en/3.0/intro/tutorial03/

--- Django_Postgresql/settings.py
+++ Django_Postgresql/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,4 +133,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Django_Postgresql.wsgi --log-file -
